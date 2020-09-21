33f08743a90971b347f60d19265daf7781c1330b
Constantin <constantin.b@gmail.com>
2020-09-21 13:14:10 +0300

    Version update

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 * Plugin URI: https://vimeotheque.com
 * Description: Add-on plugin for Vimeo Video Post PRO - Vimeo videos WordPress importer which introduces compatibility with theme JNews
 * Author: CodeFlavors
-* Version: 1.0
+* Version: 1.1
 * Author URI: https://vimeotheque.com
 */
 
